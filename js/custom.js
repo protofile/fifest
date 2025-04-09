f9fe1e18aa8ba143c2561f55f3f61f3d6c025d85
hidra <hidra.mediaart@gmail.com>
2025-04-09 09:40:47 +0700

    fix bug : - link menu - caousel width mobile view - tab day 100% mobile view - decrease padding card news mobile view

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,23 @@
 (function ($) {
 
-    "use strict";  
+    "use strict";
 
-        /* Slick carousel */
-        $('.default-carousel').slick({  
-            dots: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            centerMode: false,
-            arrows : false,
-            variableWidth: true
-          });
-  
-  
-  })(jQuery);
+    /* Slick carousel */
+    $('.default-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        arrows: false,
+        variableWidth: true
+    });
+
+    /* Active menu */
+    $('ul.navbar-nav li.nav-item .nav-link').on('click', function (e) {
+        $('ul.navbar-nav li.nav-item .nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+})(jQuery);
