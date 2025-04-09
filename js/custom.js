44d4e97f0f69edf95b315a70e5cd666bd2fb7868
hidra <hidra.mediaart@gmail.com>
2025-04-09 08:33:28 +0700

    homepage

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+    "use strict";  
+
+        /* Slick carousel */
+        $('.default-carousel').slick({  
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: false,
+            arrows : false,
+            variableWidth: true
+          });
+  
+  
+  })(jQuery);
